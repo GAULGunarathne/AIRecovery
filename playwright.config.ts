72e96d2e21ce401e866aedb4c93212b7221dd998
thara <tpshadinijk@gmail.com>
2025-03-10 23:15:48 +0530

    playwright config change

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
   },
   use: {
     launchOptions: {
-      slowMo: 1000
+      slowMo: 1500
     },
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
