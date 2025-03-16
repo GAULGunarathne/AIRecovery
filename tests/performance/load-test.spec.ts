50f4472e144e2645fe6e36c0547a450a5cb37770
thara <tpshadinijk@gmail.com>
2025-03-16 23:58:50 +0530

    script updates

--- tests/performance/load-test.spec.ts
+++ tests/performance/load-test.spec.ts
@@ -2,30 +2,19 @@ import { LoginPage } from '../../pages/loginPage';
 
 export const config = {
   target: 'https://practicesoftwaretesting.com/auth/login',
-  // phases: [
-  //   // {
-  //   //   name : 'constantArrivalRate',
-  //   //   //2 users will arrive every second for 10 seconds
-  //   //   arrivalRate: 1,
-  //   //   duration: 10
-  //   // }
-  //   // {
-  //   //   name : 'pause',
-  //   //   pause: 5
-  //   // },
-  //   {
-  //     name: 'ramp up',
-  //     //ramps up the arrival rate of virtual users from 1 to 5 over 10 seconds
-  //     duration: '5',
-  //     arrivalRate: 1,
-  //     rampTo: 2,
-  //   },
-  // ],
+  metrics: {
+    extended: true
+  },
+  phases: [
+    {
+      name: 'constantArrivalRate',
+      //2 users will arrive every second for 3 seconds
+      arrivalRate: 2,
+      duration: 3
+    }
+  ],
   engines: {
     playwright: {
-      trace: {
-        enabled: true
-      },
       launchOptions: {
         headless: true
       }
@@ -43,11 +32,11 @@ async function pageLoadAndLogin(page, vuContext, events, test) {
   const { step } = test;
   const loginPage = new LoginPage(page);
 
-  await step('landing page', async () => {
-    await page.goto('https://practicesoftwaretesting.com/auth/login');
+  await step('login-page', async () => {
+    await loginPage.loadLoginPage();
   });
 
-  await step('login', async () => {
+  await step('submt-login', async () => {
     await loginPage.Login("customer@practicesoftwaretesting.com", "welcome01");
   });
 }
